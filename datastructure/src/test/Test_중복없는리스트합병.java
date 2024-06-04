f5235a11acec4952fa6d32b8414b30d67b6ed8e6
loklok2 <loklok2@naver.com>
2024-06-04 21:33:58 +0900

    test

--- datastructure/src/test/Test_중복없는리스트합병.java
+++ datastructure/src/test/Test_중복없는리스트합병.java
@@ -9,6 +9,7 @@ package test;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
@@ -21,59 +22,94 @@ import java.util.Arrays;
 public class Test_중복없는리스트합병 {
     static ArrayList<String> removeDuplicate(ArrayList<String> al) {
         // 중복을 제거하는 함수
-        ArrayList<String> list1 = new ArrayList<>();
-        list1 = al;
-        int p = 0, q= 0;
-        while(p<list1.size()) {
-        	q = p+1;
-        	while(q <list1.size()) {
-        		if(list1.get(p).compareTo(list1.get(q))==0) {
-        			list1.remove(q);
-        		}else {
-        			break;
-        		}
-        		p++;
-        	}
-        }
-        return list1;
+    	HashSet<String> set = new HashSet<>(al);
+        return new ArrayList<>(set);
     }
 
     static List<String> mergeList(List<String> list1, List<String> list2) {
-        // 두 리스트를 병합하여 정렬하는 함수
-        List<String> mergedList = new ArrayList<>();
-        int index1 = 0; // 리스트1의 인덱스
-        int index2 = 0; // 리스트2의 인덱스
+        ArrayList<String> list3 = new ArrayList<>(); // 새로운 리스트 생성
+
+        int index1 = 0;
+        int index2 = 0;
 
         while (index1 < list1.size() && index2 < list2.size()) {
-            String str1 = list1.get(index1); // 리스트1의 현재 요소
-            String str2 = list2.get(index2); // 리스트2의 현재 요소
-            int compareResult = str1.compareTo(str2); // 두 요소 비교
+            String str1 = list1.get(index1);
+            String str2 = list2.get(index2);
 
-            if (compareResult < 0) { // 리스트1의 요소가 더 작은 경우
-                mergedList.add(str1);
+          
+            if (str1.equals(str2)) {
+                list3.add(str1); 
                 index1++;
-            } else if (compareResult > 0) { // 리스트2의 요소가 더 작은 경우
-                mergedList.add(str2);
                 index2++;
-            } else { // 두 요소가 같은 경우
-                mergedList.add(str1); // 아무 요소나 추가해도 상관 없음
+            } else if (str1.compareTo(str2) < 0) {
+                list3.add(str1);
                 index1++;
+            } else {
+                list3.add(str2);
                 index2++;
             }
         }
 
-        // 남은 요소들을 추가
+        // 남은 요소들을 추가합니다.
         while (index1 < list1.size()) {
-            mergedList.add(list1.get(index1));
+            list3.add(list1.get(index1));
             index1++;
         }
+
         while (index2 < list2.size()) {
-            mergedList.add(list2.get(index2));
+            list3.add(list2.get(index2));
             index2++;
         }
 
-        return mergedList;
+        return list3;
     }
+    static void writeFile(List<String> list3, ByteBuffer buffer) {
+		String b = " ";
+		for (String sx : list3) {
+			System.out.println(" " + sx);
+			buffer.put(sx.getBytes());
+			buffer.put(b.getBytes());
+		}
+		buffer.flip();
+	}
+	static void trimSpace(String[]arr) {
+		/*
+		 * string.trim() 사용으로 좌우 빈공백 제거
+		 */
+		for (int i = 0; i <arr.length; i++) {
+			arr[i] = arr[i].trim();
+		}
+	}
+	static void makeList(String[] sarray1, List<String>list1) {
+		/*
+		 * 배열을 list로 만드는 함수 구현 lst.add() 호출
+		 */
+		for (String temp : sarray1) {
+			list1.add(temp);
+		}
+	}
+	static void showData(String string, String[] sarray1) {
+		System.out.println(string);
+		for(String str : sarray1) {
+			System.out.print(str + " ");
+		}
+		System.out.println();
+	}
+	static void showList(String string, ArrayList<String> list1) {
+		System.out.println(string);
+		for(String str : list1) {
+			System.out.print(str);
+		}
+		System.out.println();
+	}
+	static void showList(String string, List<String> list3) {
+		System.out.println(string);
+		for(String str : list3) {
+			System.out.print(str);
+		}
+		System.out.println();
+		
+	}
 
 	public static void main(String[] args) {
 		try {
@@ -86,20 +122,20 @@ public class Test_중복없는리스트합병 {
 			 *    file2: 런던, 로마,방콕, 도쿄,서울,부산           
 			 * 자바 교재 580: Path 클래스 - 파이썬 유사 
 			 */
-			Path input1 = Paths.get("a1.txt");
+			Path input1 = Paths.get("C:/Users/loklok2/Desktop/K7_java/datastructure/a.txt");
 			byte[] bytes1 = Files.readAllBytes(input1);
 			//readAllBytes: 파일의 모든 바이트를 읽어오는 메서드입니다. 
 			//이 메서드는 파일을 열고 파일의 크기만큼 바이트를 읽어서 바이트 배열로 반환합니다.
 			System.out.println("bytes[]의 길이 = "+bytes1.length);
-			Path input2 = Paths.get("a2.txt");
+			Path input2 = Paths.get("C:/Users/loklok2/Desktop/K7_java/datastructure/a2.txt");
 			byte[] bytes2 = Files.readAllBytes(input2);
 			
 			String s1 = new String(bytes1);
 			String s2 = new String(bytes2);
 			System.out.println("입력 스트링: s1 = " + s1);
 			System.out.println("입력 스트링: s2 = " + s2);
-			String[] sarray1 = s1.split("[,\\s]+\r\n");// [,\\s]+\r\n은 쉼표나 공백이 하나 이상 나오고 이어서 캐리지 리턴과 개행 문자가 있는 패턴
-			String[] sarray2 = s2.split("[,\\s]+\r\n");//file에서 enter키는 \r\n으로 해야 분리됨
+			String[] sarray1 = s1.split("[,\\s\\r\\n]+");// [,\\s]+\r\n은 쉼표나 공백이 하나 이상 나오고 이어서 캐리지 리턴과 개행 문자가 있는 패턴
+			String[] sarray2 = s2.split("[,\\s\\r\\n]+");//file에서 enter키는 \r\n으로 해야 분리됨
 			showData("스트링 배열 sarray1", sarray1);
 			showData("스트링 배열 sarray2", sarray2);
 
@@ -131,11 +167,10 @@ public class Test_중복없는리스트합병 {
 			list1 = removeDuplicate(list1);
 			list2 = removeDuplicate(list2);
 			showList("중복 제거후 리스트1: ", list1);	
-			showList("중복 제거후 리스트1: ", list2);	
+			showList("중복 제거후 리스트2: ", list2);	
 	
 			
 			List<String> list3 = new ArrayList<>();
-			
 			// 방법3:
 			list3 = mergeList(list1, list2);
 			showList("merge후 합병리스트: ", list3);	
@@ -151,7 +186,7 @@ public class Test_중복없는리스트합병 {
 			ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
 			writeFile(list3, buffer);
 			
-			FileOutputStream file = new FileOutputStream("c.txt");
+			FileOutputStream file = new FileOutputStream("C:/Users/loklok2/Desktop/K7_java/datastructure/c.txt");
 			FileChannel channel = file.getChannel();
 			channel.write(buffer);
 			file.close();
@@ -160,48 +195,7 @@ public class Test_중복없는리스트합병 {
 		}
 	}
 
-    static void writeFile(List<String> list) {
-        try {
-            FileOutputStream file = new FileOutputStream("C:/Users/user/Desktop/K_7/k7_java/datastructure/src/c.txt");
-            FileChannel channel = file.getChannel();
-            ByteBuffer buffer = ByteBuffer.allocate(10240);
-
-            for (String str : list) {
-                buffer.put((str + "\n").getBytes());
-            }
-
-            buffer.flip();
-            channel.write(buffer);
-
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void trimSpace(String[] arr) {
-        // 문자열 배열의 공백 제거
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i].trim();
-        }
-    }
+	
     
-    static void showData(String string, String[] arr) {
-        // 주어진 메시지와 배열의 내용을 출력합니다.
-        System.out.println(string);
-        for (String str : arr) {
-            System.out.print(str + " ");
-        }
-        System.out.println();
-    }
-
-    static void showList(String string, List<String> list) {
-        // 주어진 메시지와 리스트의 내용을 출력합니다.
-        System.out.println(string);
-        for (String str : list) {
-            System.out.print(str + " ");
-        }
-        System.out.println();
-    }
 
 }
